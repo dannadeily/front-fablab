cb953a248f30a5b1fb734a0af822db9e3cf5cabf
danna duque <dannadeilydc@ufps.edu.co>
2023-06-22 10:02:30 -0500

    cambio de ruta

--- src/rutaProtegida/RutaProtegida.jsx
+++ src/rutaProtegida/RutaProtegida.jsx
@@ -5,7 +5,18 @@ import HeaderAdministrador from "../components/HeaderAdministrador";
 const RutaProtegida = ({ component: Component, ...rest }) => {
   const checkAuthentication = () => {
     const token = localStorage.getItem("token");
-    return token !== null; // Devuelve true si el token existe, de lo contrario, devuelve false
+    if (token) {
+      // Verificar la fecha de expiración del token
+      const tokenExpiration = localStorage.getItem("tokenExpiration");
+      if (tokenExpiration && new Date(tokenExpiration) < new Date()) {
+        // El token ha expirado, borrarlo y redirigir al usuario a la página de inicio
+        localStorage.removeItem("token");
+        localStorage.removeItem("tokenExpiration");
+        return false;
+      }
+      return true;
+    }
+    return false;
   };
   const isAuthenticated = checkAuthentication(); // Verificar si el token existe en el almacenamiento local
 
